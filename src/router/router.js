75d2d742097c40b7f5f33e525923922bcd3a8e0f
unknown <474692303@qq.com>
2019-01-29 20:22:08 +0800

    提交订单，订单面进不去ID错误找不到ID

--- src/router/router.js
+++ src/router/router.js
@@ -10,6 +10,22 @@ const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'sho
 const foodDetail = r => require.ensure([], () => r(require('../page/shop/children/foodDetail')), 'foodDetail')
 const shopDetail = r => require.ensure([], () => r(require('../page/shop/children/shopDetail')), 'shopDetail')
 const shopSafe = r => require.ensure([], () => r(require('../page/shop/children/children/shopSafe')), 'shopSafe')
+const order = r => require.ensure([], () => r(require('../page/order/order')), 'order')
+const orderDetail = r => require.ensure([], () => r(require('../page/order/children/orderDetail')), 'orderDetail')
+const confirmOrder = r => require.ensure([], () => r(require('../page/confirmOrder/confirmOrder')), 'confirmOrder')
+const remark = r => require.ensure([], () => r(require('../page/confirmOrder/children/remark')), 'remark')
+const payment = r => require.ensure([], () => r(require('../page/confirmOrder/children/payment')), 'payment')
+const userValidation = r => require.ensure([], () => r(require('../page/confirmOrder/children/userValidation')), 'userValidation')
+const invoice = r => require.ensure([], () => r(require('../page/confirmOrder/children/invoice')), 'invoice')
+const chooseAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/chooseAddress')), 'chooseAddress')
+const addAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/addAddress')), 'addAddress')
+const searchAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/children/searchAddress')), 'searchAddress')
+const profile = r => require.ensure([], () => r(require('../page/profile/profile')), 'profile')
+const info = r => require.ensure([], () => r(require('../page/profile/children/info')), 'info')
+const setusername = r => require.ensure([], () => r(require('../page/profile/children/children/setusername')), 'setusername')
+const address = r => require.ensure([], () => r(require('../page/profile/children/children/address')), 'address')
+const add = r => require.ensure([], () => r(require('../page/profile/children/children/children/add')), 'add')
+const addDetail = r => require.ensure([], () => r(require('../page/profile/children/children/children/children/addDetail')), 'addDetail')
 
 export default [{
     path: '/',
@@ -65,34 +81,68 @@ export default [{
                 }, ]
             }]
         },
+          //订单列表页
+          {
+            path: '/order',
+            component: order,
+            children: [{
+                path: 'orderDetail', //订单详情页
+                component: orderDetail,
+            }, ]
+        },
         //确认订单页
-        // {
-        //     path: '/confirmOrder',
-        //     component: confirmOrder,
-        //     children: [{
-        //         path: 'remark', //订单备注
-        //         component: remark,
-        //     }, {
-        //         path: 'invoice', //发票抬头
-        //         component: invoice,
-        //     }, {
-        //         path: 'payment', //付款页面
-        //         component: payment,
-        //     }, {
-        //         path: 'userValidation', //用户验证
-        //         component: userValidation,
-        //     }, {
-        //         path: 'chooseAddress', //选择地址
-        //         component: chooseAddress,
-        //         children: [{
-        //             path: 'addAddress', //添加地址
-        //             component: addAddress,
-        //             children: [{
-        //                 path: 'searchAddress', //搜索地址
-        //                 component: searchAddress,
-        //             }]
-        //         }, ]
-        //     }, ]
-        // },
+        {
+            path: '/confirmOrder',
+            component: confirmOrder,
+            children: [{
+                path: 'remark', //订单备注
+                component: remark,
+            }, {
+                path: 'invoice', //发票抬头
+                component: invoice,
+            }, {
+                path: 'payment', //付款页面
+                component: payment,
+            }, {
+                path: 'userValidation', //用户验证
+                component: userValidation,
+            }, {
+                path: 'chooseAddress', //选择地址
+                component: chooseAddress,
+                children: [{
+                    path: 'addAddress', //添加地址
+                    component: addAddress,
+                    children: [{
+                        path: 'searchAddress', //搜索地址
+                        component: searchAddress,
+                    }]
+                }, ]
+            }, ]
+        },
+           //个人信息页
+           {
+            path: '/profile',
+            component: profile,
+            children: [{
+                path: 'info', //个人信息详情页
+                component: info,
+                children: [{
+                    path: 'setusername',
+                    component: setusername,
+                },{
+                    path: 'address',
+                    component: address,     //编辑地址
+                    children:[{
+                        path:'add',
+                        component:add,
+                        children:[{
+                            path:'addDetail',
+                            component:addDetail
+                        }]
+                    }]
+                }]
+            },]
+          
+        },
     ]
 }]
